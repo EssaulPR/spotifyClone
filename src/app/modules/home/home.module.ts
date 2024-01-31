c45da5ad8b88319d700c2b943e3c89e777c8a31e
Essaul <essaul.pena.rosas@bbva.com>
2024-01-31 10:15:34 -0600

    Codigo funcionando correctamente, me falto declarar el cmp en homeModule

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '@shared/shared.module';
+import { HomepagesComponent } from './pages/homepages/homepages.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomepagesComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule,
